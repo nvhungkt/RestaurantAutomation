23a26197fddb711a5810d895f26c3f0cb2f5b1e1
hieuttse62260 <hieuperfect1997@gmail.com>
2017-06-21 17:59:14 +0700

    tạo middleservlet, staffDTO

---
+++ RestaurantAutomation/src/java/sample/tblstaff/TblStaffDTO.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.tblstaff;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TblStaffDTO implements Serializable{
+    private String id;
+    private String name;
+    private Date joinDate;
+    private String role;
+    private boolean isActive;
+
+    public TblStaffDTO() {
+    }
+
+    public TblStaffDTO(String id, String name, Date joinDate, String role, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.role = role;
+        this.isActive = isActive;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the joinDate
+     */
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    /**
+     * @param joinDate the joinDate to set
+     */
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the isActive
+     */
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive the isActive to set
+     */
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+}
